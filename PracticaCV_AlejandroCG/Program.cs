ac07ded0b5fb183610f6d21e9c92d717a3a47408
alejandro <garciacolodreroalex@outlook.com>
2023-03-14 10:15:26 +0100

    Estructura del Programa de AlejandroCG

--- PracticaCV_AlejandroCG/Program.cs
+++ PracticaCV_AlejandroCG/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //CONSTANTES
+            //VARIABLES
+            //INICIALIZAR
+            //ENTRADAS
+            //PROCESOS
+            //SALIDAS
         }
     }
 }
